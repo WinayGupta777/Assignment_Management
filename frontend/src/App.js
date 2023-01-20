3f72d1ee5c4a51712a5528453a819bd85138a67c
WinayGupta777 <vinay.gupta8101@gmail.com>
2023-01-20 20:12:10 +0530

    MUI libraries installed; Dark theme applied;

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
+import "./App.css";
+import React from 'react';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Paper } from '@mui/material';
+
+const darkTheme = createTheme({
+  palette: { mode: 'dark' }
+});
+const lightTheme = createTheme({});
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <Paper style={{ height: "100vh" }} square={true}>
+        <div className="App">
+        </div>
+      </Paper>
+    </ThemeProvider>
   );
 }
 
