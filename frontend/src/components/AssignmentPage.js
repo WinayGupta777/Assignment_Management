8fb388e523bf24efe2c87eb2a86b4308c40d7314
WinayGupta777 <vinay.gupta8101@gmail.com>
2023-01-21 19:51:07 +0530

    Demo Table is created; Specific column name given;

--- frontend/src/components/AssignmentPage.js
+++ frontend/src/components/AssignmentPage.js
@@ -1,5 +1,7 @@
-import { Box, Divider, Typography } from '@mui/material';
-import React from 'react'
+import React from 'react';
+import { Box, Divider, Typography, Paper, Table, TableCell, TableContainer, TableHead, TableRow, TableBody, Button, IconButton, Toolbar } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
 
 const AssignmentPage = () => {
     const drawerSpace = "350px";
@@ -20,9 +22,56 @@ const AssignmentPage = () => {
             >
                 <Typography variant='h3' sx={{ p: 1, pl: 3, fontFamily: 'Poppins' }}>Assignments</Typography>
                 <Divider />
+
+                <AssignmentTable />
             </Box>
         </Box>
     )
 }
 
 export default AssignmentPage;
+
+const AssignmentTable = () => {
+    return (
+        <Box
+            sx={{ flex: 1, mt: 2, backgroundColor: 'black' }}
+        >
+            <TableContainer>
+                <Table aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            {["Id", "Title", "Description", "Due Date", "Status", "Actions"].map((value, key) => (
+                                <TableCell key={key} style={{ fontSize: '20px' }}>{value}</TableCell>
+                            ))}
+                        </TableRow>
+                    </TableHead>
+
+                    <TableBody>
+                        <TableRow>
+                            <TableCell style={{ width: '5%' }}>1</TableCell>
+                            <TableCell style={{ width: '15%' }}>Assignment 1</TableCell>
+                            <TableCell style={{ width: '20%' }}>This is ....</TableCell>
+                            <TableCell style={{ width: '10%' }}>20-01-2023</TableCell>
+                            <TableCell style={{ width: '10%' }}>Inactive</TableCell>
+                            <TableCell style={{ width: '10%' }}>
+                                <Box
+                                    sx={{
+                                        display: 'flex', justifyContent: 'space-between', width: '55%',
+                                    }}
+                                >
+                                    <IconButton edge={false}>
+                                        <EditIcon />
+                                    </IconButton>
+                                    <Divider orientation='vertical' flexItem />
+                                    <IconButton>
+                                        <DeleteIcon />
+                                    </IconButton>
+                                </Box>
+                            </TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Box>
+    )
+};
