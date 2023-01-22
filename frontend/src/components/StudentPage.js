9e89591011bb4725f72d3c12444edad870d49f3d
WinayGupta777 <vinay.gupta8101@gmail.com>
2023-01-22 18:42:32 +0530

    JSON file created for Student Table content;

--- frontend/src/components/StudentPage.js
+++ frontend/src/components/StudentPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Box, Divider, Typography, Table, TableCell, TableContainer, TableHead, TableRow, TableBody } from '@mui/material';
-
+import TableData from "../json/StudentTableData.json";
 
 const StudentPage = () => {
     const drawerSpace = "350px";
@@ -22,9 +22,43 @@ const StudentPage = () => {
                 <Typography variant='h3' sx={{ p: 1, pl: 3, fontFamily: 'Poppins' }}>Students</Typography>
                 <Divider />
 
+                <StudentTable />
             </Box>
         </Box>
     )
 }
 
 export default StudentPage;
+
+
+
+const StudentTable = () => {
+  return (
+    <Box
+            sx={{ flex: 1, margin: '20px 20px', borderRadius: '5px', backgroundColor: 'black' }}
+        >
+            <TableContainer>
+                <Table aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            {["Id", "Student name", "Student email"].map((value, key) => (
+                                <TableCell key={key} style={{ fontSize: '25px' }}>{value}</TableCell>
+                            ))}
+                        </TableRow>
+                    </TableHead>
+
+                    <TableBody>
+                        {TableData.map((value, key) => (
+
+                            <TableRow key={key}>
+                                <TableCell style={{ width: '5%', fontSize: '20px' }}>{value.Id}</TableCell>
+                                <TableCell style={{ width: '15%', fontSize: '20px' }}>{value.StudentName}</TableCell>
+                                <TableCell style={{ width: '20%', fontSize: '20px' }}>{value.StudentEmail}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Box>
+  )
+};
