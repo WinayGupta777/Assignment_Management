9264dec57f80aebce3e5ae12b5884d96158fb11e
WinayGupta777 <vinay.gupta8101@gmail.com>
2023-01-22 11:41:37 +0530

    JSON file created for Submission Table content;

--- frontend/src/components/SubmissionPage.js
+++ frontend/src/components/SubmissionPage.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Box, Divider, Typography } from '@mui/material';
+import { Box, Divider, Typography, Table, TableCell, TableContainer, TableHead, TableRow, TableBody, Button, IconButton, Toolbar } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import TableData from "../json/SubmissionTableData.json";
 
 const SubmissionPage = () => {
     const drawerSpace = "350px";
@@ -20,9 +23,44 @@ const SubmissionPage = () => {
             >
                 <Typography variant='h3' sx={{ p: 1, pl: 3, fontFamily: 'Poppins' }}>Submissions</Typography>
                 <Divider />
+
+                <SubmissionTable />
             </Box>
         </Box>
     )
 }
 
 export default SubmissionPage;
+
+
+const SubmissionTable = () => {
+    return (
+        <Box
+            sx={{ flex: 1, margin: '20px 20px', borderRadius: '5px', backgroundColor: 'black' }}
+        >
+            <TableContainer>
+                <Table aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            {["Id", "Submission name", "Submitted by", "Grades"].map((value, key) => (
+                                <TableCell key={key} style={{ fontSize: '25px' }}>{value}</TableCell>
+                            ))}
+                        </TableRow>
+                    </TableHead>
+
+                    <TableBody>
+                        {TableData.map((value, key) => (
+
+                            <TableRow key={key}>
+                                <TableCell style={{ width: '5%', fontSize: '20px' }}>{value.Id}</TableCell>
+                                <TableCell style={{ width: '15%', fontSize: '20px' }}>{value.SubmissionOf}</TableCell>
+                                <TableCell style={{ width: '10%', fontSize: '20px' }}>{value.SubmittedBy}</TableCell>
+                                <TableCell style={{ width: '30%', fontSize: '20px' }}>{value.Grades}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+        </Box>
+    )
+};
