8faa0f6bfb11ce9043a5f7bece6485dcc8d7e8ba
WinayGupta777 <vinay.gupta8101@gmail.com>
2023-01-27 12:48:29 +0530

    Edit Button's functionality added; Separate state variable created for that; Bug need to be fix of 'grades';

--- frontend/src/components/student/UploadAssignment.js
+++ frontend/src/components/student/UploadAssignment.js
@@ -3,28 +3,50 @@ import { Box, Button, Checkbox, Dialog, DialogActions, DialogContent, DialogTitl
 import { Edit, Upload } from '@mui/icons-material';
 import axios from 'axios';
 
-const UploadAssignment =  (props) => {
+const UploadAssignment = (props) => {
     const [open, setOpen] = React.useState(false);
+    const [submissionDone, setSubmissionDone] = React.useState(false);
     const [formData, setFormData] = React.useState({
         id: props.row.id,
         subName: '',
+        grades: 0,
         content: '',
         student: 'Vinay Gupta' // This name will be set when Teacher logins
     });
+    const [editedFormData, setEditedFormData] = React.useState({
+        id: props.row.id,
+        subName: '',
+        grades: 0,
+        content: '',
+    });
 
-    const handleClickOpen = () => { setOpen(true); };
+    const handleClickOpen = async (button) => {
+        if (button === "edit") {
+            const res = await axios.get('http://localhost:4000/submissions/' + props.row.id);
+            setEditedFormData(res.data);
+            console.log("On edit:", res.data);
+        }
+        setOpen(true);
+    };
     const handleClose = () => { setOpen(false); };
 
-    const handleChange = (event) => {
-        setFormData({
-            ...formData,
-            [event.target.name]: event.target.value
-        });
+    const handleChange = (event, isFor) => {
+        if (isFor === "edit") {
+            setEditedFormData({
+                ...editedFormData,
+                [event.target.name]: event.target.value
+            })
+        } else {
+            setFormData({
+                ...formData,
+                [event.target.name]: event.target.value
+            });
+        }
     }
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log(formData);
+        console.log("SUBMIT -> Upload button", formData);
         // code to submit form data to the server
         axios.post("http://localhost:4000/submissions", formData)
             .then((res) => {
@@ -33,7 +55,17 @@ const UploadAssignment =  (props) => {
             }).catch((err) => console.log);
     }
 
-    const [submissionDone, setSubmissionDone] = React.useState(false);
+    const handleEditedSubmit = (event) => {
+        event.preventDefault();
+        console.log("SUBMIT -> Edit button:", editedFormData);
+        // code to submit form data to the server
+        axios.put(`http://localhost:4000/submissions/${props.row.id}`, editedFormData)
+            .then((res) => {
+                console.log("Data edited successfully !!", res.data);
+                handleClose();
+            }).catch((err) => console.log);
+    }
+
 
     useEffect(() => {
         const checkSubmission = async () => {
@@ -50,8 +82,8 @@ const UploadAssignment =  (props) => {
         }
         checkSubmission();
     }, [props.row.id])
-    
-    
+
+
     return (
         <>
             <Box
@@ -59,8 +91,8 @@ const UploadAssignment =  (props) => {
             >
                 {JSON.parse(props.row.status) ?
                     (submissionDone ?
-                        <Button onClick={handleClickOpen}> Edit &nbsp; <Edit /> </Button> :
-                        <Button onClick={handleClickOpen}> Upload &nbsp; <Upload /> </Button>
+                        <Button onClick={() => handleClickOpen("edit")}> Edit &nbsp; <Edit /> </Button> :
+                        <Button onClick={() => handleClickOpen("upload")}> Upload &nbsp; <Upload /> </Button>
                     ) :
                     <Button disabled> Upload &nbsp; <Upload /> </Button>}
             </Box>
@@ -68,21 +100,22 @@ const UploadAssignment =  (props) => {
             <Dialog open={open} onClose={handleClose}>
                 <DialogTitle sx={{ fontSize: 30 }}>Submit Assignment</DialogTitle>
                 <Divider />
-                <Box component="form" onSubmit={handleSubmit} sx={{ width: '450px' }}>
+                <Box component="form" onSubmit={submissionDone ? handleEditedSubmit : handleSubmit} sx={{ width: '450px' }}>
                     <DialogContent>
                         <Box className="row" >
                             <TextField
                                 label="Id"
                                 name="id"
                                 type="number"
-                                defaultValue={props.row.id}
-                                onChange={handleChange}
+                                value={props.row.id}
+                                onChange={submissionDone ? (e) => handleChange(e,"edit") : (e) => handleChange(e,"upload")}
                             />
                             <TextField
                                 label="Submission name"
                                 name="subName"
                                 placeholder='Assignment 1'
-                                onChange={handleChange}
+                                defaultValue={submissionDone ? editedFormData.subName : null}
+                                onChange={submissionDone ? (e) => handleChange(e,"edit") : (e) => handleChange(e,"upload")}
                             />
                         </Box>
                         <Box className="row">
@@ -92,7 +125,8 @@ const UploadAssignment =  (props) => {
                                 rows={4}
                                 label="Content"
                                 name="content"
-                                onChange={handleChange}
+                                defaultValue={submissionDone ? editedFormData.content : null}
+                                onChange={submissionDone ? (e) => handleChange(e,"edit") : (e) => handleChange(e,"upload")}
                             />
                         </Box>
                         <FormControlLabel control={<Checkbox />} label="Confirm?" name='confirm' />
@@ -103,7 +137,7 @@ const UploadAssignment =  (props) => {
                         <Button onClick={handleClose}>Cancel</Button>
                         <Button variant="contained" disableElevation type="submit"
                             sx={{ '&:hover': { color: 'black', backgroundColor: '#ffa500' } }}
-                        >Submit</Button>
+                        >{submissionDone ? "Edit" : "Upload"}</Button>
                     </DialogActions>
                 </Box>
             </Dialog>
