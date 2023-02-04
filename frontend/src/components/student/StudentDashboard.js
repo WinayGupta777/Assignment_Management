82108a38c561ca359c83356bdaa944a38d96adfd
WinayGupta777 <vinay.gupta8101@gmail.com>
2023-02-04 12:23:36 +0530

    Avatar and Name are added to the box; Also provided Tooltip;

--- frontend/src/components/student/StudentDashboard.js
+++ frontend/src/components/student/StudentDashboard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppBar, Box, Toolbar, Typography } from '@mui/material';
+import { AppBar, Box, Toolbar, Tooltip, Typography } from '@mui/material';
 import { AccountCircle } from '@mui/icons-material';
 import { Outlet } from 'react-router-dom';
 import StudentDrawer from './StudentDrawer';
@@ -16,12 +16,24 @@ const StudentDashboard = () => {
             <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
                 <Toolbar>
                     <Typography variant='h4' sx={{ flexGrow: 1 }}>Student Dashboard</Typography>
-                    <Box sx={{display: 'flex', alignItems: 'center',p: 0.5,
-                    border: '1px solid #575757', backgroundColor: '#474747', borderRadius: 1,
-                }}>
-                        <AccountCircle sx={{ fontSize: 40 }} />
-                        <Typography sx={{ fontSize: 20, ml: 2, mr: 1 }}>{currentLoggedUser.name}</Typography>
-                    </Box>
+                    <Tooltip
+                        arrow
+                        title={
+                            <Typography sx={{ fontSize: 15 }}>
+                                {currentLoggedUser.email} <br />
+                                {currentLoggedUser.role}
+                            </Typography>
+                        }
+                    >
+
+                        <Box sx={{
+                            display: 'flex', alignItems: 'center', p: 0.5, cursor: 'pointer',
+                            border: '1px solid #575757', backgroundColor: '#474747', borderRadius: 1,
+                        }}>
+                            <AccountCircle sx={{ fontSize: 40 }} />
+                            <Typography sx={{ fontSize: 20, ml: 2, mr: 1 }}>{currentLoggedUser.name}</Typography>
+                        </Box>
+                    </Tooltip>
                 </Toolbar>
             </AppBar>
             <StudentDrawer />
--- frontend/src/components/teacher/TeacherDashboard.js
+++ frontend/src/components/teacher/TeacherDashboard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppBar, Box, Toolbar, Typography } from '@mui/material';
+import { AppBar, Box, Toolbar, Tooltip, Typography } from '@mui/material';
 import { AccountCircle } from '@mui/icons-material';
 import { Outlet } from 'react-router-dom';
 import TeacherDrawer from './TeacherDrawer';
@@ -14,14 +14,25 @@ const TeacherDashboard = () => {
     return (
         <Box>
             <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-            <Toolbar>
+                <Toolbar>
                     <Typography variant='h4' sx={{ flexGrow: 1 }}>Teacher Dashboard</Typography>
-                    <Box sx={{display: 'flex', alignItems: 'center',p: 0.5,
-                    border: '1px solid #575757', backgroundColor: '#474747', borderRadius: 1,
-                }}>
-                        <AccountCircle sx={{ fontSize: 40 }} />
-                        <Typography sx={{ fontSize: 20, ml: 2, mr: 1 }}>{currentLoggedUser.name}</Typography>
-                    </Box>
+                    <Tooltip
+                        arrow
+                        title={
+                            <Typography sx={{ fontSize: 15 }}>
+                                {currentLoggedUser.email} <br />
+                                {currentLoggedUser.role}
+                            </Typography>
+                        }
+                    >
+                        <Box sx={{
+                            display: 'flex', alignItems: 'center', p: 0.5, cursor: 'pointer',
+                            border: '1px solid #575757', backgroundColor: '#474747', borderRadius: 1,
+                        }}>
+                            <AccountCircle sx={{ fontSize: 40 }} />
+                            <Typography sx={{ fontSize: 20, ml: 2, mr: 1 }}>{currentLoggedUser.name}</Typography>
+                        </Box>
+                    </Tooltip>
                 </Toolbar>
             </AppBar>
             <TeacherDrawer />
